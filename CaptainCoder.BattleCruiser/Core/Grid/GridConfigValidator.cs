f686a73c5a57fa9f2b81baf1827802455e88a387
Joseph Collard <jcollard@users.noreply.github.com>
2023-05-18 07:37:31 -0700

    chore: Refactor validator to static class.

--- CaptainCoder.BattleCruiser/Core/Grid/GridConfigValidator.cs
+++ CaptainCoder.BattleCruiser/Core/Grid/GridConfigValidator.cs
@@ -1,22 +1,35 @@
-using System.ComponentModel;
 using CaptainCoder.Core;
 namespace CaptainCoder.BattleCruiser;
 
-public class GridConfigValidator
+public static class GridConfigValidator
 {
-    public bool Validate(GridConfig config)
+    public static ValidationResult Validate(this GridConfig config)
     {
         HashSet<Position> occupied = new();
+        HashSet<ShipType> types = new ();
         bool ValidPosition(Position p) =>
             p.Row >= 0 && p.Row < config.Rows && p.Col >= 0 && p.Col < config.Cols;
         foreach (Ship ship in config.Ships)
         {
-            foreach (Position p in ship.Positions())
+            if (!types.Add(ship.ShipType)) { return new DuplicateShipResult(ship.ShipType); }
+            foreach (Position position in ship.Positions())
             {
-                if (!ValidPosition(p)) { return false; }
-                if (!occupied.Add(p)) { return false; }
+                if (!ValidPosition(position)) { return new PositionOutOfBoundsResult(position); }
+                if (!occupied.Add(position)) { return new OverlappingResult(position); }
             }
-        }        
-        return true;
+        }
+        return ValidationResult.Valid;
     }
 }
+
+public abstract record ValidationResult(bool IsValid, string Message)
+{
+    public static readonly ValidGrid Valid = new ();
+}
+public record DuplicateShipResult(ShipType Type) : 
+    ValidationResult(false, $"Grid should contain 1 boat of each type but found multiple {Type}.");
+public record PositionOutOfBoundsResult(Position Position) : 
+    ValidationResult(false, $"Position out of bounds {Position}.");
+public record OverlappingResult(Position Position) :
+    ValidationResult(false, $"Overlapping ships detected at {Position}");
+public record ValidGrid() : ValidationResult(true, "Valid Grid");
