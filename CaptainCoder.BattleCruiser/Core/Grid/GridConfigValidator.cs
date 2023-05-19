2216fb3b633b46ffec31d19d164de9c4f00a58d7
Joseph Collard <jcollard@users.noreply.github.com>
2023-05-19 08:17:06 -0700

    chore: Add expected rows / cols.

--- CaptainCoder.BattleCruiser/Core/Grid/GridConfigValidator.cs
+++ CaptainCoder.BattleCruiser/Core/Grid/GridConfigValidator.cs
@@ -3,8 +3,11 @@ namespace CaptainCoder.BattleCruiser;
 
 public static class GridConfigValidator
 {
+    public static int ExpectedRows { get; set; } = 7;
+    public static int ExpectedCols { get; set; } = 7;
     public static ValidationResult Validate(this GridConfig config)
     {
+        if (config.Rows != ExpectedRows && config.Cols != ExpectedCols) { return new InvalidDimensions(ExpectedRows, ExpectedCols); }
         if (config.Ships.Length != 3) { return new MissingShipResult(); }
         HashSet<Position> occupied = new();
         HashSet<ShipType> types = new ();
@@ -27,6 +30,8 @@ public abstract record ValidationResult(bool IsValid, string Message)
 {
     public static readonly ValidGrid Valid = new ();
 }
+public record InvalidDimensions(int rows, int cols) : 
+    ValidationResult(false, $"Grid must have {rows} rows and {cols} columns.");
 public record MissingShipResult() : 
     ValidationResult(false, $"Grid did not have exactly 3 ships.");
 public record DuplicateShipResult(ShipType Type) : 
