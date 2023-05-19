bd15e8b44bef690d13e905a9addfeaeeacae3fb5
Joseph Collard <jcollard@users.noreply.github.com>
2023-05-19 14:48:25 -0700

    chore: Add friendly assembly.

--- CaptainCoder.BattleCruiser/IsExternalInit.cs
+++ CaptainCoder.BattleCruiser/IsExternalInit.cs
@@ -1,4 +1,7 @@
+using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleTo("CaptainCoder.BattleCruiser.Tests")]
 namespace System.Runtime.CompilerServices
 {
+    
     internal static class IsExternalInit { }
 }
